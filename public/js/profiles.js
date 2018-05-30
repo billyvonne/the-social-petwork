b8d6c468f1de3d62129ea76fccfbc698e8999e27
Jess Giampietro <giampietrojess@gmail.com>
2018-05-30 19:36:28 -0400

    add basic front-end javascript

--- public/js/profiles.js
+++ public/js/profiles.js
@@ -1,1 +1,22 @@
-// Code goes here
+$(document).ready(function() {
+    $('select').material_select();
+    });
+
+
+    $(".1").click(function(){
+
+    var x = document.getElementById("myDIV2");
+    if (x.style.display !== "none") {
+        x.style.display = "none";
+    } 
+    $("#myDIV").toggle(500);
+    
+    });
+
+    $(".2").click(function(){
+        var y = document.getElementById("myDIV");
+        if (y.style.display !== "none") {
+            y.style.display = "none";
+        } 
+    $("#myDIV2").toggle(500);
+    });
