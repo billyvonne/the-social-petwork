1478426782d15c0026f65063fbdac4d19b15f8a6
Hillary Lavin <hilavin@gmail.com>
2018-05-30 20:56:25 -0400

    Add html-routes.js, require in server.js, comment out models and api-routes in server.js for testing

--- server.js
+++ server.js
@@ -3,13 +3,13 @@
 // Declare dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
-var exphbs = require("express-handlebars");
+
 
 var PORT = process.env.PORT || 8080;
 var app = express();
 
 // Require Models
-var db = require("./models");
+// var db = require("./models");
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,12 +17,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // parse application/json
 app.use(bodyParser.json());
 
-// Set-up handlebars
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
 
 // Routes //
-require("./routes/api-routes.js")(app);
+// require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
 
 // Insert static directory here (public)
 app.use(express.static("public"));
