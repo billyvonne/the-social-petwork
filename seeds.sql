a75388d92df06c6fff3b65901e3d77fa9468c602
kat thumma <katthumma@gmail.com>
2018-05-30 21:14:20 -0400

    update database and models

--- seeds.sql
+++ seeds.sql
@@ -2,9 +2,9 @@ INSERT INTO users (user_name, user_password) VALUES ('Billiam667', '2');
 INSERT INTO users (user_name, user_password) VALUES ('Hilarious68', 'password69');
 INSERT INTO users (user_name, user_password) VALUES ('KatAttack', '5oranges');
 
-INSERT INTO pets (pet_type, pet_name, user_id) VALUES ('cat', 'Dandy', 1);
-INSERT INTO pets (pet_type, pet_name, user_id) VALUES ('rat', 'Sans', 1);
-INSERT INTO pets (pet_type, pet_name, user_id) VALUES ('cat', 'Amelia', 2);
-INSERT INTO pets (pet_type, pet_name, user_id) VALUES ('cat', 'Chamblee', 2);
-INSERT INTO pets (pet_type, pet_name, user_id) VALUES ('cat', 'Tilly', 2);
-INSERT INTO pets (pet_type, pet_name, user_id) VALUES ('cat', 'Cora', 3);
+INSERT INTO pets (pet_type, pet_name, pet_age, user_id) VALUES ('cat', 'Dandy', 5, 1);
+INSERT INTO pets (pet_type, pet_name, pet_age, user_id) VALUES ('rat', 'Sans', 2, 1);
+INSERT INTO pets (pet_type, pet_name, pet_age, user_id) VALUES ('cat', 'Amelia', 4, 2);
+INSERT INTO pets (pet_type, pet_name, pet_age, user_id) VALUES ('cat', 'Chamblee', 8, 2);
+INSERT INTO pets (pet_type, pet_name, pet_age, user_id) VALUES ('cat', 'Tilly', 2, 2);
+INSERT INTO pets (pet_type, pet_name, pet_age, pet_birthday, user_id) VALUES ('cat', 'Cora', 6, 2012-04-28, 3);
