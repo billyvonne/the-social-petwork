e710f64f4df53a5ea91fff128ad43b0db7d24bd3
Jess Giampietro <giampietrojess@gmail.com>
2018-06-02 11:30:53 -0400

    update html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -31,18 +31,57 @@ app.get('/', function(req, res) {
   res.render('mainPage.html'); // load the mainPage.html file
 });
 
+app.get('/login', function(req, res) {
 
-// Login Page (/login)
+  // Don't think we need this to render, but maybe to contain other coding
+
+});
+
+// process the login form
+// app.post('/login', do all our passport stuff here);
+
+// =====================================
+// SIGNUP ==============================
+// =====================================
+// show the signup form
+app.get('/signup', function(req, res) {
+
+  // Don't think we need this to render, but maybe to contain other coding
+  
+});
+
+  // process the signup form
+  // app.post('/signup', do all our passport stuff here);
+
+// =====================================
+// PROFILE SECTION =====================
+// =====================================
+// we will want this protected so you have to be logged in to visit
+// we will use route middleware to verify this (the isLoggedIn function)
+
+app.get('/userHome', isLoggedIn, function(req, res) {
+  res.render('userHome.html', {
+      user : req.user // get the user out of session and pass to template
+  });
+});
+
+// =====================================
+// LOGOUT ==============================
+// =====================================
+app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+
+  // if user is authenticated in the session, carry on 
+  if (req.isAuthenticated())
+      return next();
+
+  // if they aren't redirect them to the home page
+  res.redirect('/');
+}
 
 
-
-// Signup Page (/signup)
-
-
-// Handle the POST for both login
-
-
-// Handle the POST for both signup
-
-
-// Profile Page (after logged in)
