133ebf6d20e4a18eb75257573f3d0bbb42cf83ba
Hillary Lavin <hilavin@gmail.com>
2018-06-03 18:08:05 -0400

    Change password to userPass

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
--- Drops the blogger if it exists currently --
+-- Drops the db if it exists currently --
 DROP DATABASE IF EXISTS petwork_db;
 -- Creates the database --
 CREATE DATABASE petwork_db;
@@ -12,7 +12,7 @@ CREATE TABLE users
     last_name varchar(20) NOT NULL,
 	username varchar(20) NOT NULL UNIQUE,
     email varchar(50) NOT NULL,
-	password varchar(20) NOT NULL,
+	userPass varchar(20) NOT NULL,
 	PRIMARY KEY (id)
 );
 
--- seeds.sql
+++ seeds.sql
@@ -1,10 +1,10 @@
-INSERT INTO users (user_name, user_password) VALUES ('Billiam667', '2');
-INSERT INTO users (user_name, user_password) VALUES ('Hilarious68', 'password69');
-INSERT INTO users (user_name, user_password) VALUES ('KatAttack', '5oranges');
+INSERT INTO users (first_name, last_name, username, email, userPass) 
+VALUES ('Billy', 'Harber', 'Billiam667', 'billy@email.com', '2'), ('Hillary', 'Lavin', 'Hilarious68', 'hillary@email.com', 'password69'), ('Kat', 'Thumma', 'KatAttack', 'kat@email.com', '5oranges');
 
-INSERT INTO pets (pet_type, pet_name, pet_age, pet_birthday, fur_color, fave_nap, fave_food, fave_toy, fave_scratch, fave_walk, fave_window, fave_feature, bowl_empty, user_id) VALUES ('cat', 'Dandy', 5, 1);
-INSERT INTO pets (pet_type, pet_name, pet_age, pet_birthday, fur_color, fave_nap, fave_food, fave_toy, fave_scratch, fave_walk, fave_window, fave_feature, bowl_empty, user_id) VALUES ('rat', 'Sans', 2, 1);
-INSERT INTO pets (pet_type, pet_name, pet_age, pet_birthday, fur_color, fave_nap, fave_food, fave_toy, fave_scratch, fave_walk, fave_window, fave_feature, bowl_empty, user_id) VALUES ('cat', 'Amelia', 4, 2);
-INSERT INTO pets (pet_type, pet_name, pet_age, pet_birthday, fur_color, fave_nap, fave_food, fave_toy, fave_scratch, fave_walk, fave_window, fave_feature, bowl_empty, user_id) VALUES ('cat', 'Chamblee', 8, 2);
-INSERT INTO pets (pet_type, pet_name, pet_age, pet_birthday, fur_color, fave_nap, fave_food, fave_toy, fave_scratch, fave_walk, fave_window, fave_feature, bowl_empty, user_id) VALUES ('cat', 'Tilly', 2, 2);
-INSERT INTO pets (pet_type, pet_name, pet_age, pet_birthday, fur_color, fave_nap, fave_food, fave_toy, fave_scratch, fave_walk, fave_window, fave_feature, bowl_empty, user_id) VALUES ('cat', 'Cora', 6, 2012-04-28, 3);
+
+-- INSERT INTO pets (pet_type, pet_name, pet_age, pet_birthday, fur_color, fave_nap, fave_food, fave_toy, fave_scratch, fave_walk, fave_window, fave_feature, bowl_empty, user_id) VALUES ('cat', 'Dandy', 5, 1);
+-- INSERT INTO pets (pet_type, pet_name, pet_age, pet_birthday, fur_color, fave_nap, fave_food, fave_toy, fave_scratch, fave_walk, fave_window, fave_feature, bowl_empty, user_id) VALUES ('rat', 'Sans', 2, 1);
+-- INSERT INTO pets (pet_type, pet_name, pet_age, pet_birthday, fur_color, fave_nap, fave_food, fave_toy, fave_scratch, fave_walk, fave_window, fave_feature, bowl_empty, user_id) VALUES ('cat', 'Amelia', 4, 2);
+-- INSERT INTO pets (pet_type, pet_name, pet_age, pet_birthday, fur_color, fave_nap, fave_food, fave_toy, fave_scratch, fave_walk, fave_window, fave_feature, bowl_empty, user_id) VALUES ('cat', 'Chamblee', 8, 2);
+-- INSERT INTO pets (pet_type, pet_name, pet_age, pet_birthday, fur_color, fave_nap, fave_food, fave_toy, fave_scratch, fave_walk, fave_window, fave_feature, bowl_empty, user_id) VALUES ('cat', 'Tilly', 2, 2);
+-- INSERT INTO pets (pet_type, pet_name, pet_age, pet_birthday, fur_color, fave_nap, fave_food, fave_toy, fave_scratch, fave_walk, fave_window, fave_feature, bowl_empty, user_id) VALUES ('cat', 'Cora', 6, 2012-04-28, 3);
