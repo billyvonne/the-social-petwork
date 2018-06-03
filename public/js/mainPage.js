10d01af77d5b8560fe91fcfe0f7437ba0c6cd3b2
Hillary Lavin <hilavin@gmail.com>
2018-06-03 17:32:06 -0400

    Change URL route for signup, added additional User params, commented out everything except new user submit

--- public/js/mainPage.js
+++ public/js/mainPage.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   // Set-up jQuery references
-  var r1 = $("#recent-pets-r1");
-  var r2 = $("#recent-pets-r2");
+//   var r1 = $("#recent-pets-r1");
+//   var r2 = $("#recent-pets-r2");
 
   // Toggle between "Sign-Up" and "Welcome Back" buttons
     $('select').material_select();
@@ -25,11 +25,11 @@ $(document).ready(function() {
     $("#myDIV2").toggle(500);
 
   // Event listeners for Register and Login buttons
-  $(document).on("submit", "#register", handleNewUserSubmit);
+  $(document).on("submit", "#signup", handleNewUserSubmit);
   $(document).on("submit", "#login", handleUserLogInSubmit);
 
   // Display Most Recent Pets
-  getRecentPets();
+//   getRecentPets();
 
   // On-click event for Register Button
         function handleNewUserSubmit(event) {
@@ -38,11 +38,12 @@ $(document).ready(function() {
             var newUser = {
                 first_name: $("#first_name").val().trim(),
                 last_name: $("#last_name").val().trim(),
+                username: $("#username").val().trim(),
                 user_email: $("#email").val().trim(),
                 user_password: $("#password").val().trim(),
             };
 
-            $.post("/api/newuser", newUser)
+            $.post("/api/signup", newUser)
                 .then(function(data) {
                     console.log(data);
                     alert("New User added. Please login!");
@@ -50,51 +51,52 @@ $(document).ready(function() {
 
             $("#first_name").val("");
             $("#last_name").val("");
+            $("#username").val("");
             $("#email").val("");
             $("#password").val("");
         }
   // On-click event for Login button
-        function handleUserLogInSubmit(event) {
-            event.preventDefault();
-        }
+        // function handleUserLogInSubmit(event) {
+        //     event.preventDefault();
+        // }
 
   // Most Recent Pets
         // A function to create a card with pet data
-        function createRecentPetCard(petData) {
-            var newCol = $("<div class='col s4'>");
-            var newCard = $("<div class='card'>");
-            newCard.append("<div class='card-image'><img src='https://www.studiblog.net/wp-content/uploads/2016/09/Unbenannt-660x330.png><a class='btn-floating halfway-fab waves-effect waves-light red><i class='material-icons'>add</i></a></div>");
-            newCard.append("<div class='card-content'><p>" + petData.pet_name + "</p></div>");
-            newCol.append(newCard);
-            return newCol;
-        }
+        // function createRecentPetCard(petData) {
+        //     var newCol = $("<div class='col s4'>");
+        //     var newCard = $("<div class='card'>");
+        //     newCard.append("<div class='card-image'><img src='https://www.studiblog.net/wp-content/uploads/2016/09/Unbenannt-660x330.png><a class='btn-floating halfway-fab waves-effect waves-light red><i class='material-icons'>add</i></a></div>");
+        //     newCard.append("<div class='card-content'><p>" + petData.pet_name + "</p></div>");
+        //     newCol.append(newCard);
+        //     return newCol;
+        // }
 
         // A function to GET most recent pets from the DB and get them ready for display
-        function getRecentPets() {
-            $.get("/api/pets", function(data) {
-                var row1 = [];
-                var row2 = [];
-                for (var i = 0; i < 6; i++) {
-                    if (i < 3) {
-                        row1.push(createRecentPetCard(data[i]));
-                    } else {
-                        row2.push(createRecentPetCard(data[i]));
-                    }
-                }
-                renderRecentPetsR1(row1);
-                renderRecentPetsR2(row2);
-            });
-        }
+        // function getRecentPets() {
+        //     $.get("/api/pets", function(data) {
+        //         var row1 = [];
+        //         var row2 = [];
+        //         for (var i = 0; i < 6; i++) {
+        //             if (i < 3) {
+        //                 row1.push(createRecentPetCard(data[i]));
+        //             } else {
+        //                 row2.push(createRecentPetCard(data[i]));
+        //             }
+        //         }
+        //         renderRecentPetsR1(row1);
+        //         renderRecentPetsR2(row2);
+        //     });
+        // }
 
         // Two functions to display most recent pets to the page
-        function renderRecentPetsR1(cards) {
-            r1.empty();
-            r1.append(cards);
-        }
-        function renderRecentPetsR2(cards) {
-            r1.empty();
-            r2.append(cards);
-        }
+        // function renderRecentPetsR1(cards) {
+        //     r1.empty();
+        //     r1.append(cards);
+        // }
+        // function renderRecentPetsR2(cards) {
+        //     r1.empty();
+        //     r2.append(cards);
+        // }
   // Featured Pet
         // A function for retrieving featured pet
 
