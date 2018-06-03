81c57a38815368ead9616d361268b94a5ff59767
Hillary Lavin <hilavin@gmail.com>
2018-06-03 17:30:48 -0400

    Comment out passport, capitalize PORT

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 // Declare dependencies
 var express  = require('express');
 var app      = express();
-var port     = process.env.PORT || 8080;
+var PORT     = process.env.PORT || 8080;
 var sequelize = require('sequelize');
 var passport = require('passport');
 var flash    = require('connect-flash');
@@ -27,15 +27,15 @@ app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.urlencoded({ extended: true })); // get information from html forms
 
 // required for passport
-app.use(session({ secret: 'fureverfriendsisnotinclusive' })); // session secret
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-app.use(flash()); // use connect-flash for flash messages stored in session
+// app.use(session({ secret: 'fureverfriendsisnotinclusive' })); // session secret
+// app.use(passport.initialize());
+// app.use(passport.session()); // persistent login sessions
+// app.use(flash()); // use connect-flash for flash messages stored in session
 
 // routes ======================================================================
-// require('../routes/api-routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
-// require('../routes/pet-api-routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
-// require("./routes/html-routes.js")(app, passport); // load our html routes and pass in our app and fully configured passport
+require('./routes/api-routes.js')(app); // load our routes and pass in our app and fully configured passport
+require('./routes/pet-api-routes.js')(app); // load our routes and pass in our app and fully configured passport
+require("./routes/html-routes.js")(app); // load our html routes and pass in our app and fully configured passport
 
 // Insert static directory here (public)
 app.use(express.static("public"));
