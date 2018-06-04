7ea43407d43f74fa86bc9812f4309f7c3d73dfea
Hillary Lavin <hilavin@gmail.com>
2018-06-03 21:01:49 -0400

    Strip down server

--- server.js
+++ server.js
@@ -2,47 +2,33 @@
 
 // Declare dependencies
 var express  = require('express');
+var sequelize = require('sequelize');
+var bodyParser   = require('body-parser');
+
 var app      = express();
 var PORT     = process.env.PORT || 8080;
-var sequelize = require('sequelize');
-var passport = require('passport');
-var flash    = require('connect-flash');
-
-var david        = require('david')
-var morgan       = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser   = require('body-parser');
-var session      = require('express-session');
 
 // Require Models
 var db = require("./models");
 
-// require('./config/passport')(passport); // pass passport for configuration
 
-// set up our express application
-app.use(morgan('dev')); // log every request to the console
-app.use(cookieParser()); // read cookies (needed for auth)
 
+// Sets up the Express app to handle data parsing
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true })); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true }));
+// parse application/json
+app.use(bodyParser.json());
 
-// required for passport
-// app.use(session({ secret: 'fureverfriendsisnotinclusive' })); // session secret
-// app.use(passport.initialize());
-// app.use(passport.session()); // persistent login sessions
-// app.use(flash()); // use connect-flash for flash messages stored in session
-
-// routes ======================================================================
-require('./routes/api-routes.js')(app); // load our routes and pass in our app and fully configured passport
-// require('./routes/pet-api-routes.js')(app); // load our routes and pass in our app and fully configured passport
-require("./routes/html-routes.js")(app); // load our html routes and pass in our app and fully configured passport
-
-// Insert static directory here (public)
+// Static directory
 app.use(express.static("public"));
 
+// Routes ======================================================================
+require('./routes/api-routes.js')(app);
+// require('./routes/pet-api-routes.js')(app);
+require("./routes/html-routes.js")(app);
 
 
-// launch ======================================================================
+// Launch ======================================================================
 app.listen(PORT, function() {
 console.log("The magic happens on PORT" + PORT);
 });
