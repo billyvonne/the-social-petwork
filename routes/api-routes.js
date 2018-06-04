2a63f1dfb732e3fa84cd32895886723f158f3965
Hillary Lavin <hilavin@gmail.com>
2018-06-04 18:11:13 -0400

    Change /newuser to /signup and start /login

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,27 +4,8 @@ var db = require("../models");
 
 // Wrap all routes in a module.exports function
 module.exports = function(app) {
-    // // GET all users
-    // app.get("/api/user", function(req,res) {
-    //     db.User.findAll({
-    //     }).then(function(dbUser){
-    //         res.json(dbUser);
-    //     });
-    // });
-    // // GET specific user by id
-    // app.get("/api/user/:id", function(req,res) {
-    //     db.User.findOne({
-    //         where:  {
-    //             id: req.params.id
-    //         },
-    //         include: [db.Pet]
-    //     }).then(function(dbUser){
-    //         res.json(dbUser);
-    //     });
-    // });
-    // POST routes
-    // Create New User
-    app.post("/api/signup", function(req, res){
+    // Create new user
+    app.post("/signup", function(req, res){
         db.User.create({
             first_name: req.body.first_name,
             last_name: req.body.last_name,
@@ -35,9 +16,17 @@ module.exports = function(app) {
             res.json(dbUser);
         });
     });
-    
-    // PUT
-    // DELETE
+    // Login existing user - needs a GET and POST
+    app.get("/login", function(req,res) {
+        db.User.findOne({
+            where:  {
+                id: req.params.id
+            },
+            include: [db.Pet]
+        }).then(function(dbUser){
+            res.json(dbUser);
+        });
+    });
 };
 
 
