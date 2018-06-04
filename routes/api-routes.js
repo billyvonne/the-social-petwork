98872a97a8967ee11c3184ab3db5e7a35355fd76
Hillary Lavin <hilavin@gmail.com>
2018-06-03 21:02:52 -0400

    Comment out all except POST

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,24 +4,24 @@ var db = require("../models");
 
 // Wrap all routes in a module.exports function
 module.exports = function(app) {
-    // GET all users
-    app.get("/api/user", function(req,res) {
-        db.User.findAll({
-        }).then(function(dbUser){
-            res.json(dbUser);
-        });
-    });
-    // GET specific user by id
-    app.get("/api/user/:id", function(req,res) {
-        db.User.findOne({
-            where:  {
-                id: req.params.id
-            },
-            include: [db.Pet]
-        }).then(function(dbUser){
-            res.json(dbUser);
-        });
-    });
+    // // GET all users
+    // app.get("/api/user", function(req,res) {
+    //     db.User.findAll({
+    //     }).then(function(dbUser){
+    //         res.json(dbUser);
+    //     });
+    // });
+    // // GET specific user by id
+    // app.get("/api/user/:id", function(req,res) {
+    //     db.User.findOne({
+    //         where:  {
+    //             id: req.params.id
+    //         },
+    //         include: [db.Pet]
+    //     }).then(function(dbUser){
+    //         res.json(dbUser);
+    //     });
+    // });
     // POST routes
     // Create New User
     app.post("/api/signup", function(req, res){
@@ -29,8 +29,8 @@ module.exports = function(app) {
             first_name: req.body.first_name,
             last_name: req.body.last_name,
             username: req.body.username,
-            email: req.body.email,
-            userPass: req.body.userPass
+            user_email: req.body.email,
+            user_pass: req.body.user_pass
         }).then(function(dbUser) {
             res.json(dbUser);
         });
