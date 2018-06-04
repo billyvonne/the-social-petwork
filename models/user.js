2c126cc7f1d34af468a5a464d93461e631887ff2
Hillary Lavin <hilavin@gmail.com>
2018-06-04 18:09:59 -0400

    Add unique param, doesn't work yet

--- models/user.js
+++ models/user.js
@@ -29,6 +29,7 @@ module.exports = function(sequelize, DataTypes) {
       user_email: {
         type: DataTypes.STRING,
         allowNull: false,
+        unique: true,
         validate: {
           len: [1]
         }
