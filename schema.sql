b9f21550629ba33598908de0fe3d16ed3fee8b55
Jess Giampietro <giampietrojess@gmail.com>
2018-06-05 23:44:23 -0400

    uncomment pets table in schema, add createdat and updated at

--- schema.sql
+++ schema.sql
@@ -41,23 +41,25 @@ CREATE TABLE likes
 );
 
 
--- CREATE TABLE pets
--- (
--- 	id int NOT NULL AUTO_INCREMENT,
--- 	pet_type VARCHAR(25) NOT NULL,
---     pet_name VARCHAR(40) NOT NULL,
---     pet_age int(3), 
---     pet_birthday DATE,
---     fur_color VARCHAR(100),
---     fave_nap VARCHAR(100),
---     fave_food VARCHAR(100),
---     fave_toy VARCHAR(100),
---     fave_scratch VARCHAR(100),
---     fave_walk VARCHAR(100),
---     fave_window VARCHAR(100),
---     fave_feature VARCHAR(100),
---     bowl_empty VARCHAR(100),
--- 	user_id int NOT NULL,
--- 	PRIMARY KEY (id),
--- 	FOREIGN KEY (user_id) REFERENCES users(id)
--- );
+CREATE TABLE pets
+(
+	id int NOT NULL AUTO_INCREMENT,
+	pet_type VARCHAR(25) NOT NULL,
+    pet_name VARCHAR(40) NOT NULL,
+    pet_age int(3), 
+    pet_birthday DATE,
+    fur_color VARCHAR(100),
+    fave_nap VARCHAR(100),
+    fave_food VARCHAR(100),
+    fave_toy VARCHAR(100),
+    fave_scratch VARCHAR(100),
+    fave_walk VARCHAR(100),
+    fave_window VARCHAR(100),
+    fave_feature VARCHAR(100),
+    bowl_empty VARCHAR(100),
+	user_id int NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (user_id) REFERENCES users(id),
+    createdAt timestamp,
+    updatedAt datetime,
+);
